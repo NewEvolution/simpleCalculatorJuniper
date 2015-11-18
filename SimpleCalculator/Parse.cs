bc31a28405f8659a73c765a81708ce2c199bf08b
Ryan Tanay <ryantanay@newevolution.org>
2015-11-18 16:06:23 -0600

    Started on the parser, but then the rules changed, so nothing is functional.

---
+++ SimpleCalculator/Parse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Parse
+    {
+        public string[] Convert(string input)
+        {
+            string[] validOperands = new string[] { "*", "%", "/", "+", "-", "=" };
+            string[] validSigns = new string[] { "+", "-" };
+            string nope = "Input is invalid";
+            char[] allChars = input.ToCharArray();
+            char toRemove = ' ';
+            allChars = allChars.Where(character => character != toRemove).ToArray();
+            StringBuilder firstArg = new StringBuilder();
+            StringBuilder secondArg = new StringBuilder();
+            for (int i = 0; i < allChars.Length; i++)
+            {
+                string character = allChars[i].ToString();
+                if (!character.All(char.IsLetterOrDigit) && !validOperands.Contains(character))
+                {
+                    throw new ArgumentException(nope);
+                }
+                if (i == 0)
+                {
+                    if (!character.All(char.IsLetterOrDigit) && !validSigns.Contains(character))
+                    {
+                        throw new ArgumentException(nope);
+                    }
+                    else
+                    {
+                        firstArg.Append(allChars[i]);
+                    }
+                }
+                else if (validSigns.Contains(allChars[i - 1].ToString()))
+                {
+
+                }
+            }
+            if(validSigns.Contains(allChars[0].ToString()))
+            {
+
+            }
+            string[] output = new string[3];
+            if (output.Length != 3) throw new ArgumentException(nope);
+            int firstArg;
+            int secondArg;
+            bool firstArgIsInt = int.TryParse(output[0], out firstArg);
+            bool secondArgIsInt = int.TryParse(output[2], out secondArg);
+            if (!firstArgIsInt && (output[0].Length > 1 || !output[0].All(char.IsLetter))) throw new ArgumentException(nope);
+            if (output[1].Length > 1 || !validOperands.Contains(output[1])) throw new ArgumentException(nope);
+            if (!secondArgIsInt && (output[2].Length > 1 || !output[2].All(char.IsLetter))) throw new ArgumentException(nope);
+            return output;
+        }
+    }
+}
---
+++ SimpleCalculatorTests/ParseTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using SimpleCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculatorTests
+{
+    [TestClass]
+    public class ParseTests
+    {
+        [TestMethod]
+        public void ParseCanBeInstantiated()
+        {
+            Parse parser = new Parse();
+            Assert.IsNotNull(parser);
+        }
+
+        [TestMethod]
+        public void ParseHasConvertMethodThatTakesStringAndReturnsCharArray()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1 + 2");
+            string[] expected = new string[] { "1", "+", "2" };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseConvertThowsErrorWithIncompleteInput()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1 +");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseConvertThowsErrorWithExtraInput()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1 + 1 - 4");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseConvertThrowsErrorWithInvalidFirstArgument()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1b + 1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseConvertThrowsErrorWithInvalidLastArgument()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1 + cc");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ParseConvertThrowsErrorWithInvalidOperator()
+        {
+            Parse parser = new Parse();
+            string[] actual = parser.Convert("1 # 1");
+        }
+
+        [TestMethod]
+        public void ParseConvertAcceptsSingleLetterArguments()
+        {
+            Parse parser = new Parse();
+            string[] expected = new string[] { "1", "+", "c" };
+            string[] actual = parser.Convert("1 + c");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ParseConvertAcceptsLargerNumberArguments()
+        {
+            Parse parser = new Parse();
+            string[] expected = new string[] { "g", "+", "235" };
+            string[] actual = parser.Convert("g + 235");
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
